c5bce5e70aaa7c98bd00e88e8d94c86cfea4b18a
Slowlyo <1821725017@qq.com>
2022-09-06 17:51:59 +0800

    恢复jquery

--- src/Traits/Assets.php
+++ src/Traits/Assets.php
@@ -16,6 +16,7 @@ trait Assets
     private static array $js = [
         'vendor/admin/static/amis/sdk.js',
         'vendor/admin/static/js/history.js',
+        'vendor/admin/static/js/jquery.js',
         'vendor/admin/static/js/axios.min.js',
     ];
 
