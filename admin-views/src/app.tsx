efd43f80606d58088d2cf7ac3160dd0d93c04d65
Slowlyo <1821725017@qq.com>
2022-11-12 17:56:55 +0800

    disableMobile

--- admin-views/src/app.tsx
+++ admin-views/src/app.tsx
@@ -92,6 +92,7 @@ export const layout: RunTimeLayoutConfig = ({initialState, setInitialState}) => 
         links: [],
         breadcrumbRender: () => undefined,
         menuHeaderRender: undefined,
+        disableMobile: true,
         // 自定义 403 页面
         // unAccessible: <div>unAccessible</div>,
         childrenRender: (children, props) => {
