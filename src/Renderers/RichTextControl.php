234287b94c50a01f037a032129c884cd52e6557c
Slowlyo <1821725017@qq.com>
2023-04-09 21:39:03 +0800

    菜单收起样式优化

--- src/Renderers/RichTextControl.php
+++ src/Renderers/RichTextControl.php
@@ -9,11 +9,11 @@ use Slowlyo\OwlAdmin\Traits\Uploader;
  *
  * @method self static($value) 是否静态展示
  * @method self staticInputClassName($value) 静态展示表单项Value类名
- * @method self staticSchema($value) 
+ * @method self staticSchema($value)
  * @method self value($value) 默认值，切记只能是静态值，不支持取变量，跟数据关联是通过设置 name 属性来实现的。
  * @method self width($value) 在Table中调整宽度
  * @method self placeholder($value) 占位符
- * @method self validations($value) 
+ * @method self validations($value)
  * @method self id($value) 组件唯一 id，主要用于日志采集
  * @method self remark($value) 显示一个小图标, 鼠标放上去的时候显示提示内容
  * @method self hint($value) 输入提示，聚焦的时候显示
@@ -21,22 +21,22 @@ use Slowlyo\OwlAdmin\Traits\Uploader;
  * @method self readOnlyOn($value) 只读条件
  * @method self horizontal($value) 当配置为水平布局的时候，用来配置具体的左右分配。
  * @method self validateApi($value) 远端校验表单项接口
- * @method self vendor($value) 编辑器类型 可选值: froala | tinymce | 
- * @method self borderMode($value) 边框模式，全边框，还是半边框，或者没边框。 可选值: full | half | none | 
+ * @method self vendor($value) 编辑器类型 可选值: froala | tinymce |
+ * @method self borderMode($value) 边框模式，全边框，还是半边框，或者没边框。 可选值: full | half | none |
  * @method self required($value) 是否为必填
  * @method self clearValueOnHidden($value) 表单项隐藏时，是否在当前 Form 中删除掉该表单项值。注意同名的未隐藏的表单项值也会删掉
  * @method self onEvent($value) 事件动作配置
  * @method self staticOn($value) 是否静态展示表达式
- * @method self size($value) 表单项大小 可选值: xs | sm | md | lg | full | 
+ * @method self size($value) 表单项大小 可选值: xs | sm | md | lg | full |
  * @method self name($value) 字段名，表单提交时的 key，支持多层级，用.连接，如： a.b.c
- * @method self desc($value) 
+ * @method self desc($value)
  * @method self inputClassName($value) 配置 input className
  * @method self receiver($value) 图片保存 API
  * @method self videoReceiver($value) 视频保存 API
  * @method self disabled($value) 是否禁用
  * @method self staticPlaceholder($value) 静态展示空值占位
  * @method self readOnly($value) 是否只读
- * @method self mode($value) 配置当前表单项展示模式 可选值: normal | inline | horizontal | 
+ * @method self mode($value) 配置当前表单项展示模式 可选值: normal | inline | horizontal |
  * @method self disabledOn($value) 是否禁用表达式
  * @method self visibleOn($value) 是否显示表达式
  * @method self staticLabelClassName($value) 静态展示表单项Label类名
