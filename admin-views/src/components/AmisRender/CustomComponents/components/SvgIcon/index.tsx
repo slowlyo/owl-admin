4d2dd967ed0d553d1cdf6eac582912e77c0cfa32
Slowlyo <slowlyo_email@qq.com>
2023-08-29 15:37:50 +0800

    更改前端

--- admin-views/src/components/AmisRender/CustomComponents/components/SvgIcon/index.tsx
+++
@@ -1,6 +0,0 @@
-import React, {forwardRef} from "react"
-import {Icon} from "@iconify/react"
-
-const SvgIcon = forwardRef((props: any, ref) => <Icon icon={props.icon} className={props.className}/>)
-
-export default SvgIcon
--- admin-views/src/service/request/index.ts
+++
@@ -1,5 +0,0 @@
-import CustomAxiosInstance from "@/service/request/instance"
-import config from "./config"
-
-
-export const request = new CustomAxiosInstance(config).instance
