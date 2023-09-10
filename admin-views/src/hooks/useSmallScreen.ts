0cb01b448980612db8e9abd31ed089291d24eec7
Slowlyo <slowlyo_email@qq.com>
2023-09-10 21:33:53 +0800

    ant design pro

--- admin-views/src/hooks/useSmallScreen.ts
+++
@@ -1,5 +0,0 @@
-import {useSize} from 'ahooks'
-
-export const useSmallScreen = () => {
-    return useSize(document.body)?.width < 768
-}
--- admin-views/src/layouts/components/LayoutTabs/index.tsx
+++
@@ -1,7 +0,0 @@
-export const LayoutTabs = () => {
-    return (
-        <>
-            LayoutTabs
-        </>
-    )
-}
