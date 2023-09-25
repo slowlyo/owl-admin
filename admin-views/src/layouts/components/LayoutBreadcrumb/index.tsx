e4060caa8e9ecc685a94e76840f638c517ec0df2
Slowlyo <slowlyo_email@qq.com>
2023-09-25 22:59:27 +0800

    设置项

--- admin-views/src/layouts/components/LayoutBreadcrumb/index.tsx
+++ admin-views/src/layouts/components/LayoutBreadcrumb/index.tsx
@@ -41,7 +41,6 @@ const LayoutBreadcrumb = () => {
             let menu = null
             if (route.children?.length) {
                 menu = getDropMenu(route)
-                menu.theme = 'dark'
             }
 
             let item = {title: route.meta?.title} as any
