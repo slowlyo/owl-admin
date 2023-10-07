fdf20208449e660af2afcc5d9b4dc017cc57102f
Slowlyo <slowlyo_email@qq.com>
2023-10-07 21:47:05 +0800

    footer class name

--- admin-views/src/layouts/components/LayoutFooter/index.tsx
+++ admin-views/src/layouts/components/LayoutFooter/index.tsx
@@ -8,7 +8,7 @@ const LayoutFooter = () => {
     }
 
     return (
-        <div className="h-[65px] flex items-center justify-center bg-white border-t">
+        <div className="owl-footer h-[65px] flex items-center justify-center bg-white border-t">
             <div dangerouslySetInnerHTML={{__html: getSetting('layout.footer')}}></div>
         </div>
     )
