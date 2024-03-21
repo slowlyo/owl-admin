6fdbfbf6c8dcd96416c6dbd71b5100c5fa2cc35b
slowlyo <slowlyo_email@qq.com>
2024-03-21 23:57:51 +0800

    回到顶部按钮

--- admin-views/src/layouts/components/LayoutContent/index.tsx
+++ admin-views/src/layouts/components/LayoutContent/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo} from 'react'
+import React, {useEffect, useMemo, useRef, useState} from 'react'
 import {Redirect, Route, Switch} from 'react-router-dom'
 import useRoute from '@/routes'
 import lazyLoad from '@/utils/lazyload'
@@ -10,9 +10,12 @@ import useSetting from '@/hooks/useSetting'
 import LayoutTabs from '@/layouts/components/LayoutTabs'
 import LayoutFooter from '@/layouts/components/LayoutFooter'
 import {Scrollbars} from 'react-custom-scrollbars'
+import {FloatButton} from 'antd'
 
 // 内容区域
 const LayoutContent = () => {
+    const scrollbarRef = useRef<any>()
+    const [scroll, setScroll] = useState(null)
     const {getSetting} = useSetting()
     const {routes, defaultRoute, getCurrentRoute} = useRoute()
     const flattenRoutes = useMemo(() => getFlattenRoutes(routes) || [], [routes])
@@ -34,6 +37,11 @@ const LayoutContent = () => {
         }
     }
 
+    // 回到顶部
+    const backTop = () => {
+        scrollbarRef?.current?.scrollToTop()
+    }
+
     // 处理 keep-alive 导致 modal 无法关闭的问题
     const clearModal = () => {
         if (getSetting('system_theme_setting.keepAlive')) {
@@ -50,10 +58,13 @@ const LayoutContent = () => {
 
 
     return (
-        <div className="h-full flex flex-col bg-[var(--owl-body-bg)]">
+        <div className="h-full flex flex-col bg-[var(--owl-body-bg)]" id="xxxa">
             {(getSetting('system_theme_setting.enableTab') && !currentRoute?.is_full) && <LayoutTabs/>}
             <div className="flex-1">
-                <Scrollbars autoHide className="clear-children-mb">
+                <Scrollbars autoHide
+                            className="clear-children-mb"
+                            ref={scrollbarRef}
+                            onScroll={() => setScroll(scrollbarRef.current.getValues().scrollTop)}>
                     <div className="owl-container">
                         <QueueAnim className="relative"
                                    type={[getSetting('system_theme_setting.animateInType'), getSetting('system_theme_setting.animateOutType')]}
@@ -61,13 +72,13 @@ const LayoutContent = () => {
                             <div key={pathname} id={pathname} className="absolute w-full iframe">
                                 <Switch location={location}>
                                     {flattenRoutes.map(({name, path, component}, index) => {
-                                        return <Route key={index}  path={path.replace(/\?.*$/, '')}  render={() => (
+                                        return <Route key={index} path={path.replace(/\?.*$/, '')} render={() => (
                                             // @ts-ignore
                                             <KeepAlive name={name}
                                                        cacheKey={path}
                                                        id={`keep-alive-${name}-${path}`} // 确保ID唯一
-                                                       when={currentRoute?.keep_alive  == 1 || (getSetting('system_theme_setting.keepAlive') && getSetting('layout.keep_alive_exclude')?.indexOf(path) == -1)}>
-                                                {React.createElement(component, { currentRoute })}
+                                                       when={currentRoute?.keep_alive == 1 || (getSetting('system_theme_setting.keepAlive') && getSetting('layout.keep_alive_exclude')?.indexOf(path) == -1)}>
+                                                {React.createElement(component, {currentRoute})}
                                             </KeepAlive>
                                         )}/>
                                     })}
@@ -80,6 +91,8 @@ const LayoutContent = () => {
                                 </Switch>
                             </div>
                         </QueueAnim>
+
+                        {scroll > 400 && <FloatButton.BackTop visibilityHeight={0} onClick={() => backTop()}/>}
                     </div>
                 </Scrollbars>
             </div>
