6854d4eadea4dde19556ba21242a3938dc14d458
slowlyo <slowlyo_email@qq.com>
2024-03-29 23:52:52 +0800

    update

---
+++ .phpstorm.meta.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace PHPSTORM_META {
+    // expectedArguments(\amis(), 0, 'input-text', 'input-array', 'input-textarea', 'input-password', 'input-number', 'input-radio', 'input-checkbox', 'input-switch', 'input-date', 'input-datetime', 'input-time', 'input-color', 'input-file');
+}
