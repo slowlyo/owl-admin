075f1a0dead9ba884f44150086484707c0774ba1
Slowlyo <slowlyo_email@qq.com>
2024-04-15 14:29:50 +0800

    优化动态js加载逻辑

--- admin-views/src/utils/dynamicAssets.ts
+++ admin-views/src/utils/dynamicAssets.ts
@@ -1,35 +1,35 @@
 export function dynamicAssetsHandler({js = [], css = [], styles = [], scripts = []}) {
-    const appendToHead = (element: any) => document.getElementsByTagName("head")[0].appendChild(element)
+    const appendToHead = (element: any) => document.getElementsByTagName('head')[0].appendChild(element)
 
     const loadJS = (src: string) => {
-        const script = document.createElement("script")
+        const script = document.createElement('script')
         script.src = src
-        script.type = "text/javascript"
+        script.type = 'text/javascript'
         appendToHead(script)
     }
 
     const loadCSS = (href: string) => {
-        const link = document.createElement("link")
+        const link = document.createElement('link')
         link.href = href
-        link.rel = "stylesheet"
+        link.rel = 'stylesheet'
         appendToHead(link)
     }
 
-    const loadScripts = (arr: string[]) => {
-        const script = document.createElement("script")
-        script.innerHTML = arr.join("")
-        script.type = "text/javascript"
+    const loadScripts = (js: string) => {
+        const script = document.createElement('script')
+        script.innerHTML = js
+        script.type = 'text/javascript'
         appendToHead(script)
     }
 
-    const loadStyles = (arr: string[]) => {
-        const style = document.createElement("style")
-        style.innerHTML = arr.join("")
+    const loadStyles = (css: string) => {
+        const style = document.createElement('style')
+        style.innerHTML = css
         appendToHead(style)
     }
 
-    js.forEach(js => loadJS(js))
-    css.forEach(css => loadCSS(css))
-    scripts.length && loadScripts(scripts)
-    styles.length && loadStyles(styles)
+    js?.forEach(js => loadJS(js))
+    css?.forEach(css => loadCSS(css))
+    scripts?.forEach(val => loadScripts(val))
+    styles?.forEach(val => loadStyles(val))
 }
