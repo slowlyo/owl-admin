4758a84cbb68fed23a1fb7c74ac1cf8232dea4fe
Slowlyo <slowlyo_email@qq.com>
2024-05-28 20:41:17 +0800

    feat: 首页样式优化

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -90,7 +90,7 @@ JS
                     ->items([
                         amis()->Image()->src(url(Admin::config('admin.logo'))),
                         amis()->Wrapper()->className('text-3xl mt-9 font-bold')->body(Admin::config('admin.name')),
-                        amis()->Flex()->className('px-24 w-full mt-5')->justify('space-around')->items([
+                        amis()->Flex()->className('w-full mt-5')->justify('center')->items([
                             $link('GitHub', 'https://github.com/slowlyo/owl-admin'),
                             $link('Official website', 'https://owladmin.com'),
                             $link('Documentation', 'https://doc.owladmin.com'),
